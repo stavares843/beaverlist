212f8a5f743d535d9e3b7ea2b61f7394609d1acc
Sara Tavares <saratavares843@gmail.com>
2023-03-21 14:18:06 +0000

    add tests

--- beaverlistTests/beaverlistTests.swift
+++ beaverlistTests/beaverlistTests.swift
@@ -6,31 +6,30 @@
 //
 
 import XCTest
+import UIKit
+import SwiftUI
+
 @testable import Beaverlist
 
-class beaverlistTests: XCTestCase {
+class ContentViewTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  // Test case to verify if the view is rendered
+  func testContentView_Rendered() {
+    let contentView = ContentView()
+    let hostingController = UIHostingController(rootView: contentView)
+    XCTAssertNotNil(hostingController.view)
+  }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  // Test case to verify if the user inputs are loaded correctly from the txt file
+  func testContentView_UserInputsLoaded() {
+    let contentView = ContentView()
+    XCTAssertFalse(contentView.userInputs.isEmpty)
+  }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  // Test case to verify if the search functionality is working correctly
+  func testContentView_SearchFunctionality() {
+    let contentView = ContentView()
+    contentView.searchText = "input"
+    XCTAssertFalse(contentView.filteredUserInputs.isEmpty)
+  }
 }
