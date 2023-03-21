1b7f77f5571e0e46793023550b7d0395163e8896
Sara Tavares <saratavares843@gmail.com>
2023-03-21 13:09:36 +0000

    initial commit

---
+++ beaverlist/ContentView.swift
@@ -0,0 +1,268 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var searchText = ""
+    @State private var showGradientPicker = false
+    @State private var selectedGradient = Color.blueberryBlush
+    @State private var isTapped: Bool = false
+
+    let userInputs: [String]
+
+    init() {
+        guard let url = Bundle.main.url(forResource: "user_inputs", withExtension: "txt"),
+              let data = try? String(contentsOf: url) else {
+            print("Failed to load user inputs from txt file.")
+            self.userInputs = []
+            return
+        }
+
+        let regex = try! NSRegularExpression(pattern: #"user_input_\d+ = "(.*?)""#)
+        self.userInputs = regex.matches(in: data, range: NSRange(data.startIndex..., in: data)).map {
+            String(data[Range($0.range(at: 1), in: data)!])
+        }
+    }
+
+    var filteredUserInputs: [String] {
+        if searchText.isEmpty {
+            return userInputs
+        } else {
+            return userInputs.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("User Inputs")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                Spacer()
+
+                Menu {
+                    Button(action: {
+                        selectedGradient = Color.blueberryBlush
+                    }) {
+                        Text("Blueberry Blush")
+                        Image("blueberry_blush")
+                    }
+                    
+                    Button(action: {
+                        selectedGradient = Color.slateMist
+                    }) {
+                        Text("Slate Mist")
+                        Image("slate_mist")
+                    }
+                    
+                    Button(action: {
+                        selectedGradient = Color.mysticTwilight
+                    }) {
+                        Text("Mystic Twilight")
+                        Image("mystic_twilight")
+                    }
+                    
+                    Button(action: {
+                        selectedGradient = Color.sunsetBlaze
+                    }) {
+                        Text("Sunset Blaze")
+                        Image("sunset_blaze")
+                    }
+                } label: {
+                    Image("themes")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .font(.system(size: 15))
+                }
+                .padding(.trailing, 16)
+            }
+
+            SearchBar(text: $searchText, placeholder: "Search")
+                .padding(.horizontal, 16)
+
+            if filteredUserInputs.isEmpty {
+                Text("No results found for '\(searchText)' 🦫")
+                    .padding()
+            } else {
+                ScrollView {
+                    LazyVStack(alignment: .center, spacing: 1) {
+                        ForEach(filteredUserInputs, id: \.self) { input in
+                            HStack {
+                                Spacer()
+                                Text(input)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.vertical, 10)
+                                    .padding(.horizontal, 5)
+                                    .foregroundColor(.white)
+                                    .background(selectedGradient)
+                                    .cornerRadius(10)
+                                    .onTapGesture {
+                                        let fullString = input
+                                        var textToCopy = ""
+                                        
+                                        if let startRange = fullString.range(of: ">>"),
+                                           let endRange = fullString.range(of: "<<", range: startRange.upperBound..<fullString.endIndex) {
+                                            textToCopy = String(fullString[startRange.upperBound..<endRange.lowerBound])
+                                        } else if let range = fullString.range(of: ":") {
+                                            textToCopy = String(fullString[range.upperBound...])
+                                        }
+                                        
+
+                                            // Copy text to clipboard
+                                            UIPasteboard.general.string = textToCopy.trimmingCharacters(in: .whitespacesAndNewlines)
+                                            print("Copied to clipboard: \(textToCopy)")
+                                         }
+                                
+                                
+                                }
+                                Spacer()
+                            }
+                        .padding(.trailing, 10)
+                        }
+                    }
+                
+                }
+            }
+            Spacer()
+        }
+
+/*
+struct GradientPicker: View {
+    let colors: [Color]
+    let startPoint: UnitPoint
+    let endPoint: UnitPoint
+    let onSelectGradient: (LinearGradient) -> Void
+
+    @State private var isPickerPresented = false
+
+    var body: some View {
+        Button(action: {
+            isPickerPresented.toggle()
+        }) {
+            Image(systemName: "paintpalette")
+                .font(.system(size: 25))
+        }
+        .padding(.trailing, 16)
+        .popover(isPresented: $isPickerPresented) {
+            VStack(spacing: 16) {
+                Text("Choose a gradient")
+                    .font(.headline)
+
+                    ForEach(0..<colors.count) { index in
+                        Button(action: {
+                            let gradient = LinearGradient(
+                                gradient: Gradient(colors: [colors[index], colors[(index + 1) % colors.count]]),
+                                startPoint: startPoint,
+                                endPoint: endPoint)
+                            onSelectGradient(gradient)
+                            isPickerPresented.toggle()
+                        }) {
+                            HStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(LinearGradient(
+                                        gradient: Gradient(colors: [colors[index], colors[(index + 1) % colors.count]]),
+                                        startPoint: startPoint,
+                                        endPoint: endPoint))
+                                    .frame(width: 30, height: 30)
+                                Text("\(colors[index].description) - \(colors[(index + 1) % colors.count].description)")
+                            }
+                        }
+                    }
+                }
+        }
+    }
+} */
+/*
+    struct SearchBar: UIViewRepresentable {
+        @Binding var text: String
+        let placeholder: String
+
+        class Coordinator: NSObject, UISearchBarDelegate {
+            @Binding var text: String
+
+            init(text: Binding<String>) {
+                _text = text
+            }
+
+            func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+                text = searchText
+                
+                // Show cancel button when user starts searching
+                searchBar.showsCancelButton = true
+            }
+
+            func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+                searchBar.showsCancelButton = false
+                searchBar.resignFirstResponder()
+                text = ""
+            }
+        }
+
+        func makeCoordinator() -> Coordinator {
+            return Coordinator(text: $text)
+        }
+
+        func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+            let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: 100, height: 44))
+            searchBar.delegate = context.coordinator
+            searchBar.placeholder = placeholder
+            searchBar.showsCancelButton = false // Hide cancel button initially
+            return searchBar
+        }
+
+        func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+            uiView.text = text
+        }
+    }
+    }
+*/
+    
+    struct SearchBar: UIViewRepresentable {
+        @Binding var text: String
+        let placeholder: String
+
+        class Coordinator: NSObject, UISearchBarDelegate {
+            @Binding var text: String
+
+            init(text: Binding<String>) {
+                _text = text
+            }
+
+            func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+                text = searchText
+                
+                // Show cancel button when user starts searching
+                searchBar.showsCancelButton = true
+            }
+
+            func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+                searchBar.showsCancelButton = false
+                searchBar.resignFirstResponder()
+                text = ""
+            }
+            
+            func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+                searchBar.showsCancelButton = true
+            }
+            
+            func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+                searchBar.showsCancelButton = false
+            }
+        }
+
+        func makeCoordinator() -> Coordinator {
+            return Coordinator(text: $text)
+        }
+
+        func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+            let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: 100, height: 44))
+            searchBar.delegate = context.coordinator
+            searchBar.placeholder = placeholder
+            searchBar.showsCancelButton = false // Hide cancel button initially
+            return searchBar
+        }
+
+        func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+            uiView.text = text
+        }
+    }
+    }
---
+++ beaverlist/beaverlist.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct beaverlist: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
