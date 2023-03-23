2ab17949080786d8415529f9c57597553cd6926f
Sara Tavares <saratavares843@gmail.com>
2023-03-23 13:23:21 +0000

    fix indentation

--- Colors.swift
+++ Colors.swift
@@ -1,57 +1,57 @@
 import SwiftUI
 
 extension Color {
-    init(hex: UInt, opacity: Double = 1) {
-        let red = Double((hex >> 16) & 0xff) / 255
-        let green = Double((hex >> 8) & 0xff) / 255
-        let blue = Double((hex >> 0) & 0xff) / 255
+  init(hex: UInt, opacity: Double = 1) {
+    let red = Double((hex >> 16) & 0xff) / 255
+    let green = Double((hex >> 8) & 0xff) / 255
+    let blue = Double((hex >> 0) & 0xff) / 255
 
-        self.init(
-            .sRGB,
-            red: red,
-            green: green,
-            blue: blue,
-            opacity: opacity
-        )
-    }
-
-    static let neonBlue = Color(hex: 0xfc5c65)
-
-    static let customGradient = LinearGradient(
-            gradient: Gradient(colors: [Color(hex: 0x3a6186), Color(hex: 0x89253e)]),
-            startPoint: .topLeading,
-            endPoint: .bottomTrailing
-        )
-    static let slateMist = LinearGradient(
-            gradient: Gradient(colors: [Color(hex: 0x4ECDC4), Color(hex: 0x556270)]),
-            startPoint: .topLeading,
-            endPoint: .bottomTrailing
-        )
-
-    static let honeyDew = LinearGradient(
-            gradient: Gradient(colors: [Color(hex: 0x43C6AC), Color(hex: 0xF8FFAE)]),
-            startPoint: .topLeading,
-            endPoint: .bottomTrailing
-        )
-
-    static let blueberryBlush = LinearGradient(
-        gradient: Gradient(colors: [.blue, .purple]),
-        startPoint: .topLeading,
-        endPoint: .bottomTrailing
+    self.init(
+      .sRGB,
+      red: red,
+      green: green,
+      blue: blue,
+      opacity: opacity
     )
+  }
 
-    static let mysticTwilight =  LinearGradient(
-        gradient: Gradient(
-        colors: [Color(hex: 0x3a6186), Color(hex: 0x89253e)]),
-        startPoint: .leading,
-        endPoint: .trailing
-    )
+  static let neonBlue = Color(hex: 0xfc5c65)
 
-    static let sunsetBlaze =  LinearGradient(
-        gradient: Gradient(
-        colors: [.red, .yellow]),
-        startPoint: .topLeading,
-        endPoint: .bottomTrailing
-    )
+  static let customGradient = LinearGradient(
+    gradient: Gradient(colors: [Color(hex: 0x3a6186), Color(hex: 0x89253e)]),
+    startPoint: .topLeading,
+    endPoint: .bottomTrailing
+  )
+  static let slateMist = LinearGradient(
+    gradient: Gradient(colors: [Color(hex: 0x4ECDC4), Color(hex: 0x556270)]),
+    startPoint: .topLeading,
+    endPoint: .bottomTrailing
+  )
+
+  static let honeyDew = LinearGradient(
+    gradient: Gradient(colors: [Color(hex: 0x43C6AC), Color(hex: 0xF8FFAE)]),
+    startPoint: .topLeading,
+    endPoint: .bottomTrailing
+  )
+
+  static let blueberryBlush = LinearGradient(
+    gradient: Gradient(colors: [.blue, .purple]),
+    startPoint: .topLeading,
+    endPoint: .bottomTrailing
+  )
+
+  static let mysticTwilight = LinearGradient(
+    gradient: Gradient(
+      colors: [Color(hex: 0x3a6186), Color(hex: 0x89253e)]),
+    startPoint: .leading,
+    endPoint: .trailing
+  )
+
+  static let sunsetBlaze = LinearGradient(
+    gradient: Gradient(
+      colors: [.red, .yellow]),
+    startPoint: .topLeading,
+    endPoint: .bottomTrailing
+  )
 
 }
--- beaverlistUITests/BeaverlistUITests.swift
+++ beaverlistUITests/BeaverlistUITests.swift
@@ -9,57 +9,72 @@ import XCTest
 
 class BeaverlistUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-            continueAfterFailure = false
-            let app = XCUIApplication()
-            app.launch()
-        }
+  override func setUpWithError() throws {
+    continueAfterFailure = false
+    let app = XCUIApplication()
+    app.launch()
+  }
 
-        func testSearchWithNoResults() throws {
-            let app = XCUIApplication()
-            let searchField = app.searchFields.firstMatch
-            app.searchFields["Search"].tap()
-            searchField.typeText("76")
-            app.staticTexts["No results found for '76' 🦫"].tap()
+  func testSearchWithNoResults() throws {
+    let app = XCUIApplication()
+    let searchField = app.searchFields.firstMatch
+    app.searchFields["Search"].tap()
+    searchField.typeText("76")
+    app.staticTexts["No results found for '76' 🦫"].tap()
 
-        }
+  }
 
-        func testSearchWithResults() throws {
-            let app = XCUIApplication()
-            let searchField = app.searchFields.firstMatch
-            searchField.tap()
-            searchField.typeText("Japan")
-            app.staticTexts["This line includes a word from the Japanese writing system: こんにちは"].tap()
-        }
+  func testSearchWithResults() throws {
+    let app = XCUIApplication()
+    let searchField = app.searchFields.firstMatch
+    searchField.tap()
+    searchField.typeText("Japan")
+    app.staticTexts["This line includes a word from the Japanese writing system: こんにちは"].tap()
+  }
 
-    func testThemesMenu() throws {
-        let app = XCUIApplication()
-        let themesButton = app.buttons["themes"]
-        themesButton.tap()
-        let collectionViewsQuery = app.collectionViews
-        collectionViewsQuery/*@START_MENU_TOKEN@*/.buttons["Blueberry Blush"]/*[[".cells.buttons[\"Blueberry Blush\"]",".buttons[\"Blueberry Blush\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        themesButton.tap()
-        collectionViewsQuery/*@START_MENU_TOKEN@*/.buttons["Slate Mist"]/*[[".cells.buttons[\"Slate Mist\"]",".buttons[\"Slate Mist\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        themesButton.tap()
-        collectionViewsQuery/*@START_MENU_TOKEN@*/.buttons["Mystic Twilight"]/*[[".cells.buttons[\"Mystic Twilight\"]",".buttons[\"Mystic Twilight\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        themesButton.tap()
-        app.collectionViews/*@START_MENU_TOKEN@*/.buttons["Sunset Blaze"]/*[[".cells.buttons[\"Sunset Blaze\"]",".buttons[\"Sunset Blaze\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-    }
+  func testThemesMenu() throws {
+    let app = XCUIApplication()
+    let themesButton = app.buttons["themes"]
+    themesButton.tap()
+    let collectionViewsQuery = app.collectionViews
+    collectionViewsQuery /*@START_MENU_TOKEN@*/.buttons[
+      "Blueberry Blush"
+    ] /*[[".cells.buttons[\"Blueberry Blush\"]",".buttons[\"Blueberry Blush\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+      .tap()
+    themesButton.tap()
+    collectionViewsQuery /*@START_MENU_TOKEN@*/.buttons[
+      "Slate Mist"
+    ] /*[[".cells.buttons[\"Slate Mist\"]",".buttons[\"Slate Mist\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+      .tap()
+    themesButton.tap()
+    collectionViewsQuery /*@START_MENU_TOKEN@*/.buttons[
+      "Mystic Twilight"
+    ] /*[[".cells.buttons[\"Mystic Twilight\"]",".buttons[\"Mystic Twilight\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+      .tap()
+    themesButton.tap()
+    app.collectionViews /*@START_MENU_TOKEN@*/.buttons[
+      "Sunset Blaze"
+    ] /*[[".cells.buttons[\"Sunset Blaze\"]",".buttons[\"Sunset Blaze\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+      .tap()
+  }
 
-    func testDismissSearch() throws {
-        let app = XCUIApplication()
-        let searchField = app.searchFields.firstMatch
-        searchField.tap()
-        searchField.typeText("test dismissing search")
-        searchField.buttons["Clear text"].tap()
-        searchField.typeText("test dismissing search - cancel")
-        app/*@START_MENU_TOKEN@*/.staticTexts["Cancel"]/*[[".buttons[\"Cancel\"].staticTexts[\"Cancel\"]",".staticTexts[\"Cancel\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-    }
+  func testDismissSearch() throws {
+    let app = XCUIApplication()
+    let searchField = app.searchFields.firstMatch
+    searchField.tap()
+    searchField.typeText("test dismissing search")
+    searchField.buttons["Clear text"].tap()
+    searchField.typeText("test dismissing search - cancel")
+    app /*@START_MENU_TOKEN@*/.staticTexts[
+      "Cancel"
+    ] /*[[".buttons[\"Cancel\"].staticTexts[\"Cancel\"]",".staticTexts[\"Cancel\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+      .tap()
+  }
 
-    func testShowToast() throws {
-        let app = XCUIApplication()
-        app.staticTexts["This string contains trademark symbol: ™"].tap()
-        // Check if the toast is displayed
-        app.staticTexts["Copied to clipboard"].tap()
-    }
+  func testShowToast() throws {
+    let app = XCUIApplication()
+    app.staticTexts["This string contains trademark symbol: ™"].tap()
+    // Check if the toast is displayed
+    app.staticTexts["Copied to clipboard"].tap()
+  }
 }
